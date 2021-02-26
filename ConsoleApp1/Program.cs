8d147b6bf3366c42a9c10588bf8155a3fc97a9b6
Ricardo Vieira dos santos <ricardovieirasantos22@hotmail.com>
2021-02-26 15:40:37 -0300

    inciando projeto..

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o numero da Conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("digite o nome do usuario: ");
+            string titular = Console.ReadLine();
+
+
+
+        }
+    }
+}
